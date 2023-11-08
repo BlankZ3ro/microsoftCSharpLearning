9576c4fdc75c13578c3cbaaccfdf4660896115a9
BlankZ3ro <jbmcmath2@gmail.com>
2023-11-08 12:13:34 -0600

    small additions
    added some smaller changes and notes on logical OR operator(s).

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -8,8 +8,15 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
+if((roll1 ==roll2) || (roll2 == roll3) || (roll1 == roll3)) //Combined three Boolean expressions to create one composite Boolean expression, sometimes known as a "compound condition"
+//the || are logical OR operators, these basically say "either the expression to my left OR the expression to my right must be true in order for the entire expression to be true."
 
-if (total > 14)
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if (total >= 15)
 {
     Console.WriteLine("You win!");
 }
@@ -18,5 +25,7 @@ if (total < 15)
 {
     Console.WriteLine("You lost!");
 }
+
+
 
 
