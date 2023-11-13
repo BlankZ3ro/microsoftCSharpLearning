06f0b6c88cdba016952961b3018742aaf1c75621
BlankZ3ro <jbmcmath2@gmail.com>
2023-11-12 23:22:38 -0600

    Beginning challenge
    Start of Exercise - Complete a challenge activity to apply business rules

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -12,20 +12,31 @@ if((roll1 ==roll2) || (roll2 == roll3) || (roll1 == roll3)) //Combined three Boo
 //the || are logical OR operators, these basically say "either the expression to my left OR the expression to my right must be true in order for the entire expression to be true."
 
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+   if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new car!");
 }
-
-if (total < 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You lost!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-
-
-
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
---
+++ CsharpProjects/TestProject/ProgramTwo.cs
@@ -0,0 +1,10 @@
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration > 10)
+{
+    
+}
+
+
